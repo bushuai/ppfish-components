9dab1e1e87ccf7c44aab3da54974653d9916ce38
bushuai <ibushuai@gmail.com>
2020-12-04 15:09:46 +0800

    refactor(ts): ImageLoader

--- source/components/ImageLoader/ImageLoader.tsx
+++ source/components/ImageLoader/ImageLoader.tsx
@@ -1,75 +1,97 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import omit from 'omit.js';
-import {polyfill} from 'react-lifecycles-compat';
+import React from "react";
+import PropTypes from "prop-types";
+import omit from "omit.js";
+import { polyfill } from "react-lifecycles-compat";
 
 const defaultSizeMap = {
   small: {
     width: 90,
-    height: 60,
+    height: 60
   },
   default: {
     width: 150,
-    height: 100,
+    height: 100
   },
   large: {
     width: 240,
-    height: 160,
+    height: 160
   }
 };
 
 const Status = {
-  PENDING: 'pending',
-  LOADING: 'loading',
-  LOADED: 'loaded',
-  FAILED: 'failed',
-};
-const noop = () => {
+  PENDING: "pending",
+  LOADING: "loading",
+  LOADED: "loaded",
+  FAILED: "failed"
 };
 
-class ImageLoader extends React.Component {
+type StatusValue = "pending" | "loading" | "loaded" | "failed";
+
+const noop = () => {};
+
+interface ImageLoaderProps {
+  className?: string;
+  prefixCls?: string;
+  placeholderSize?: string | object;
+  style?: React.CSSProperties;
+  preLoader?: React.ReactNode | (() => React.ReactNode);
+  failedLoader?: React.ReactNode | (() => React.ReactNode);
+  src: string;
+  onLoad?: (event: Event) => void;
+  onError?: (error: Error) => void;
+  imgProps?: object;
+}
+
+interface ImageLoaderState {
+  status?: StatusValue;
+  prevProps?: ImageLoaderProps;
+}
+
+class ImageLoader extends React.Component<ImageLoaderProps, ImageLoaderState> {
   static propTypes = {
     className: PropTypes.string,
     prefixCls: PropTypes.string,
-    placeholderSize: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object,
-    ]),
+    placeholderSize: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
     style: PropTypes.object,
-    preLoader: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.func,
-    ]),
-    failedLoader: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.func,
-    ]),
+    preLoader: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+    failedLoader: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
     src: PropTypes.string,
     onLoad: PropTypes.func,
     onError: PropTypes.func,
-    imgProps: PropTypes.object,
+    imgProps: PropTypes.object
   };
 
   static defaultProps = {
-    prefixCls: 'fishd-image-loader',
-    placeholderSize: 'default',
+    prefixCls: "fishd-image-loader",
+    placeholderSize: "default",
     onLoad: noop,
-    onError: noop,
+    onError: noop
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const {prevProps = {}} = prevState;
-    const newState = {prevProps: nextProps};
+  static getDerivedStateFromProps: React.GetDerivedStateFromProps<
+    ImageLoaderProps,
+    ImageLoaderState
+  > = (nextProps, prevState) => {
+    const { prevProps = { src: "" } } = prevState;
+    const newState: ImageLoaderState = { prevProps: nextProps };
     if (prevProps.src !== nextProps.src) {
-      newState.status = nextProps.src ? Status.LOADING : Status.PENDING;
+      newState.status = (nextProps.src
+        ? Status.LOADING
+        : Status.PENDING) as StatusValue;
     }
 
     return newState;
-  }
+  };
 
-  constructor(props) {
+  private img: HTMLImageElement;
+
+  constructor(props: ImageLoaderProps) {
     super(props);
-    this.state = {status: props.src ? Status.LOADING : Status.PENDING, prevProps: props};
+
+    this.state = {
+      status: (props.src ? Status.LOADING : Status.PENDING) as StatusValue,
+      prevProps: props
+    };
   }
 
   componentDidMount() {
@@ -89,7 +111,7 @@ class ImageLoader extends React.Component {
   }
 
   getClassName() {
-    const {prefixCls} = this.props;
+    const { prefixCls } = this.props;
     let className = `${prefixCls} ${prefixCls}-${this.state.status}`;
     if (this.props.className) {
       className = `${className} ${this.props.className}`;
@@ -97,23 +119,21 @@ class ImageLoader extends React.Component {
     return className;
   }
 
-  base64Img = (cls) => {
-    const {placeholderSize} = this.props;
+  base64Img = cls => {
+    const { placeholderSize } = this.props;
     let sizeProps = {
-      width: '100%'
+      width: "100%"
     };
-    if (typeof placeholderSize === 'object') {
+    if (typeof placeholderSize === "object") {
       sizeProps = Object.assign({}, sizeProps, placeholderSize);
-    } else if (['small', 'default', 'large'].indexOf(placeholderSize) > -1) {
+    } else if (["small", "default", "large"].indexOf(placeholderSize) > -1) {
       sizeProps = Object.assign({}, sizeProps, defaultSizeMap[placeholderSize]);
     }
-    return (
-      <div className={cls} style={{...sizeProps}}/>
-    );
-  }
+    return <div className={cls} style={{ ...sizeProps }} />;
+  };
 
   createLoader() {
-    this.destroyLoader();  // We can only have one loader at a time.
+    this.destroyLoader(); // We can only have one loader at a time.
 
     this.img = new Image();
     this.img.onload = this.handleLoad.bind(this);
@@ -131,31 +151,30 @@ class ImageLoader extends React.Component {
 
   handleLoad(event) {
     this.destroyLoader();
-    this.setState({status: Status.LOADED});
+    this.setState({ status: Status.LOADED as StatusValue });
 
     if (this.props.onLoad) this.props.onLoad(event);
   }
 
   handleError(error) {
     this.destroyLoader();
-    this.setState({status: Status.FAILED});
+    this.setState({ status: Status.FAILED as StatusValue });
 
     if (this.props.onError) this.props.onError(error);
   }
 
   renderImg() {
-    const {src, imgProps} = this.props;
-    const otherProps = omit(imgProps, [
-      'src',
-    ]);
+    const { src, imgProps } = this.props;
+    const otherProps = omit(imgProps, ["src"]);
 
     return <img src={src} {...otherProps} />;
   }
 
   render() {
-    const {style, preLoader, failedLoader} = this.props;
+    const { style, preLoader, failedLoader } = this.props;
     let wrapperProps = {
-      className: this.getClassName(),
+      style: {},
+      className: this.getClassName()
     };
 
     if (style) {
@@ -173,34 +192,33 @@ class ImageLoader extends React.Component {
       case Status.FAILED:
         // 使用自定义加载失败图片
         if (failedLoader) {
-          if (typeof failedLoader === 'function') {
+          if (typeof failedLoader === "function") {
             content = failedLoader();
           } else {
             content = failedLoader;
           }
           // 使用系统预置加载失败图片
         } else {
-          content = this.base64Img('failed-img');
+          content = this.base64Img("failed-img");
         }
         break;
 
       default:
         // 使用自定义占位图片
         if (preLoader) {
-          if (typeof preLoader === 'function') {
+          if (typeof preLoader === "function") {
             content = preLoader();
           } else {
             content = preLoader;
           }
           // 使用系统预置占位图片
         } else {
-          content = this.base64Img('preload-img');
+          content = this.base64Img("preload-img");
         }
         break;
     }
 
     return <div {...wrapperProps}>{content}</div>;
-
   }
 }
 
